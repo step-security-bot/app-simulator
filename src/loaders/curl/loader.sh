ca2998cccefb31752ef2c6d568e463a066ca7114
Severin Neumann <neumanns@cisco.com>
2024-12-17 10:20:31 +0100

    fix apk in curl loader (#29)
    Signed-off-by: svrnm <neumanns@cisco.com>

--- src/loaders/curl/loader.sh
+++ src/loaders/curl/loader.sh
@@ -8,7 +8,6 @@ while true; do
 	ID=$(uuidgen)
 	for URL in ${URLS}; do
 		/usr/bin/curl -s "${URL}?unique_session_id=${ID}"
-		#/usr/bin/curl -s -X POST -d "unique_session_id=${ID}" "${URL}"
 	done
 	sleep "${SLEEP}"
 done
